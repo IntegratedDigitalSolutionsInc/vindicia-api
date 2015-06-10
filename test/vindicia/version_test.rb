02d4caa00f59603f19e69f132b28ad4e4e8fdb11
Jens Thomsen <jens@integrated-digital.com>
2015-06-10 10:57:27 -0600

    Add CashBox 4.3 and 9.0 support

--- test/vindicia/version_test.rb
+++ test/vindicia/version_test.rb
@@ -3,6 +3,6 @@ require 'net/http'
 
 class Vindicia::VersionTest < Test::Unit::TestCase
   def test_is_the_correct_version
-    assert_equal Vindicia::VERSION, '0.1.0'
+    assert_equal Vindicia::VERSION, '0.1.1'
   end
 end
